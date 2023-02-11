3049991313df8d1cf47ea260c3cad3a4ce90253b
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-11 14:58:43 +0100

    first commit

---
+++ src/main/java/boccarusso/BoccarussoApplication.java
@@ -0,0 +1,13 @@
+package boccarusso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoccarussoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoccarussoApplication.class, args);
+	}
+
+}
