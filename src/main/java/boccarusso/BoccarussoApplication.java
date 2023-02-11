af8bf3c3bc5a7cad84292b08468adae8213fc87a
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-11 18:55:30 +0100

    added sanitization bean into application

--- src/main/java/boccarusso/BoccarussoApplication.java
+++ src/main/java/boccarusso/BoccarussoApplication.java
@@ -2,12 +2,20 @@ package boccarusso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BoccarussoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoccarussoApplication.class, args);
+	}
+
+	@Bean
+	public static String sanitize(String s) {
+		return s
+				.replaceAll(" ", "-")
+				.replaceAll("&", "and");
 	}
 
 }
