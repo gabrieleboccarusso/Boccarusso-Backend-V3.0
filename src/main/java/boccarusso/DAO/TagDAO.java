810ee3b9582f37af64271ec8135a4cc61f63c2fb
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-17 22:49:05 +0100

    finished the tag DAO

--- src/main/java/boccarusso/DAO/TagDAO.java
+++ src/main/java/boccarusso/DAO/TagDAO.java
@@ -15,8 +15,35 @@ public class TagDAO implements DAO<Tag> {
   return this.tagRepository.findById(id);
  }
 
+ public Iterable<Tag> getAll() {
+  return this.tagRepository.findAll();
+ }
+
  public Tag getExisting(String id) {
   return this.get(id).get();
  }
 
+ public Tag save(Tag t) {
+  return this.tagRepository.save(t);
+ }
+
+ public boolean exists(String id) {
+  return this.tagRepository.existsById(id);
+ }
+
+ public boolean delete(String id) {
+  boolean flag = true;
+
+  this.tagRepository.deleteById(id);
+
+  if (this.exists(id)) {
+   flag = false;
+  }
+
+  return flag;
+ }
+
+ public void deleteExisting(String id) {
+  this.tagRepository.deleteById(id);
+ }
 }
