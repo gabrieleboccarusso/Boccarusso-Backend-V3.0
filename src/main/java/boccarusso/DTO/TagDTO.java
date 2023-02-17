4ec95f8a2e0c1eb66eed14124ce654e733f2492e
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-17 23:09:19 +0100

    made DTO for tag

---
+++ src/main/java/boccarusso/DTO/TagDTO.java
@@ -0,0 +1,12 @@
+package boccarusso.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagDTO {
+ private String name;
+}
