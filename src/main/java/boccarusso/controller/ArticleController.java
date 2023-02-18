2a088011d64fc0d5547abbfae71a7a19c58e5ce8
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-18 18:53:27 +0100

    added post of article in both controller and service

--- src/main/java/boccarusso/controller/ArticleController.java
+++ src/main/java/boccarusso/controller/ArticleController.java
@@ -1,10 +1,14 @@
 package boccarusso.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import boccarusso.DTO.ArticleDTO;
 import boccarusso.entity.Article;
 import boccarusso.service.ArticleService;
 
@@ -17,5 +21,10 @@ public class ArticleController {
  @GetMapping
  Iterable<Article> getAllArticle() {
   return this.articleService.getAllArticles();
+ }
+
+ @PostMapping
+ ResponseEntity<Article> postArticle(@RequestBody ArticleDTO dto) {
+  return this.articleService.postArticle(dto);
  }
 }
--- src/main/java/boccarusso/service/ArticleService.java
+++ src/main/java/boccarusso/service/ArticleService.java
@@ -1,9 +1,12 @@
 package boccarusso.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import boccarusso.DAO.ArticleDAO;
+import boccarusso.DTO.ArticleDTO;
 import boccarusso.entity.Article;
 
 @Service
@@ -13,5 +16,19 @@ public class ArticleService {
 
  public Iterable<Article> getAllArticles() {
   return this.ArticleDao.getAll();
+ }
+
+ public ResponseEntity<Article> postArticle(ArticleDTO dto) {
+  Article result = new Article(dto);
+  HttpStatus status = HttpStatus.BAD_REQUEST;
+
+  if (!this.ArticleDao.exists(result.getSlug())) {
+   result = this.ArticleDao.save(result);
+   status = HttpStatus.OK;
+  } else {
+   result = null;
+  }
+
+  return new ResponseEntity<Article>(result, status);
  }
 }
