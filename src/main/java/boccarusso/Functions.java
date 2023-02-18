b11cb0501eb7cb7c3217f5e2269ee8c1d2581689
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-18 16:16:41 +0100

    added current date function

--- src/main/java/boccarusso/Functions.java
+++ src/main/java/boccarusso/Functions.java
@@ -1,9 +1,16 @@
 package boccarusso;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Functions {
   public static String sanitize(String s) {
     return s
         .replaceAll(" ", "-")
         .replaceAll("&", "and");
+  }
+
+  public static String getCurrentDate() {
+    return LocalDate.now().format(DateTimeFormatter.ofPattern("dd MMM yyyy"));
   }
 }
