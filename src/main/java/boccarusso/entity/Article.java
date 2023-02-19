2d5b7b74caad58c9313e7edf7020f49217a73b41
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-19 10:27:40 +0100

    added tags to article entity

--- src/main/java/boccarusso/entity/Article.java
+++ src/main/java/boccarusso/entity/Article.java
@@ -3,9 +3,16 @@ package boccarusso.entity;
 import static boccarusso.Functions.getCurrentDate;
 import static boccarusso.Functions.sanitize;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boccarusso.DTO.ArticleDTO;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,6 +30,10 @@ public class Article {
  private String content;
  private String creationDate;
  private String lastUpdate;
+
+ @ManyToMany(fetch = FetchType.EAGER)
+ @JoinTable(name = "article_tag", joinColumns = @JoinColumn(name = "article_slug"), inverseJoinColumns = @JoinColumn(name = "tag_slug"))
+ private List<Tag> tags = new ArrayList<>();
 
  public Article(ArticleDTO dto) {
   this.slug = sanitize(dto.getTitle());
