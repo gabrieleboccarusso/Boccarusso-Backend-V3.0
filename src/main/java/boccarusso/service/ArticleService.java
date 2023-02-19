c568eb65b2f72a817a44579dde4a20ad364b8fc5
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-19 11:13:22 +0100

    added tag validator to article service

--- src/main/java/boccarusso/service/ArticleService.java
+++ src/main/java/boccarusso/service/ArticleService.java
@@ -1,5 +1,8 @@
 package boccarusso.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -7,14 +10,28 @@ import org.springframework.stereotype.Service;
 import boccarusso.DAO.ArticleDAO;
 import boccarusso.DTO.ArticleDTO;
 import boccarusso.entity.Article;
+import boccarusso.entity.Tag;
 
 @Service
 public class ArticleService extends SuperService<Article> {
  @Autowired
  ArticleDAO ArticleDao;
 
+ private void tagsValidator(Article a, List<String> tags) {
+  Optional<Tag> tag;
+
+  for (int i = 0; i < tags.size(); ++i) {
+   tag = this.ArticleDao.checkTagExistence(tags.get(i));
+
+   if (!tag.isEmpty()) {
+    a.addTag(tag.get());
+   }
+  }
+ }
+
  public ResponseEntity<Article> post(ArticleDTO dto) {
   Article article = new Article(dto);
+  this.tagsValidator(article, dto.getTagSlugs());
 
   return super.post(article, article.getSlug());
  }
