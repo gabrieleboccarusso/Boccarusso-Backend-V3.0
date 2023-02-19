e0ea15e903168809f192f2768bd0266c9617d2ba
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-19 10:46:16 +0100

    made method to check if a tag exists

--- src/main/java/boccarusso/DAO/ArticleDAO.java
+++ src/main/java/boccarusso/DAO/ArticleDAO.java
@@ -1,17 +1,26 @@
 package boccarusso.DAO;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import boccarusso.entity.Article;
+import boccarusso.entity.Tag;
 import boccarusso.repository.ArticleRepository;
 
 @Component
 public class ArticleDAO extends SuperDAO<Article> {
  @Autowired
  private ArticleRepository articleRepository;
+ @Autowired
+ private TagDAO tagDao;
 
  public Iterable<Article> getArticlesByTitle(String title) {
   return this.articleRepository.findByTitleContaining(title);
+ }
+
+ public Optional<Tag> checkTagExistence(String id) {
+  return this.tagDao.get(id);
  }
 }
