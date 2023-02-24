ad89c3134c1702b21b9106d0991df45fde8b6eac
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-24 17:13:45 +0100

    added CORS configuration

--- src/main/java/boccarusso/BoccarussoApplication.java
+++ src/main/java/boccarusso/BoccarussoApplication.java
@@ -2,6 +2,9 @@ package boccarusso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BoccarussoApplication {
@@ -9,4 +12,14 @@ public class BoccarussoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoccarussoApplication.class, args);
 	}
+
+	@Bean
+ public WebMvcConfigurer corsConfigurer() {
+  return new WebMvcConfigurer() {
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*");
+   }
+  };
+ }
 }
