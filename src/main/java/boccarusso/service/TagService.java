345c366fd15b9b5a61685f0c2792f803e410498f
gabrieleboccarusso <boccarussodev@gmail.com>
2023-03-05 19:07:13 +0000

    removed tag dao autowiring in tag service

--- src/main/java/boccarusso/service/TagService.java
+++ src/main/java/boccarusso/service/TagService.java
@@ -1,27 +1,22 @@
 package boccarusso.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import boccarusso.DAO.TagDAO;
 import boccarusso.DTO.TagDTO;
 import boccarusso.entity.Tag;
 
 @Service
 public class TagService extends SuperService<Tag, String> {
- @Autowired
- private TagDAO tagDao;
-
  public ResponseEntity<Tag> post(TagDTO tagDto) {
   Tag tag = new Tag(tagDto);
 
-  return super.post(tag, tag.getSlug());
+  return super.post(new Tag(tagDto), tag.getSlug());
  }
 
  public ResponseEntity<Tag> PatchTagName(String id, String value) {
   return super.patch(id, (Tag t) -> {
-   this.tagDao.deleteExisting(id);
+   this.dao.deleteExisting(id);
    t.setName(value);
    t.setSlug(value);
    return t;
